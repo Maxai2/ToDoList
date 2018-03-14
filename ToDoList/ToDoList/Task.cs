a3bdc902111795b6912ad3ac584bd1477f44fde5
Хамелеон <maxali946@gmail.com>
2018-03-14 16:59:43 +0400

    20%

---
+++ ToDoList/ToDoList/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//----------------------------------
+namespace ToDoList
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public string Deadline { get; set; }
+        public string Comment { get; set; }
+    }
+}
+//----------------------------------
