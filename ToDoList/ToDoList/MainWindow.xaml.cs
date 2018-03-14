3f0f66dffd492fbd1dfa8dcbbda1743c463e6571
Хамелеон <maxali946@gmail.com>
2018-03-14 18:16:11 +0400

    35%

--- ToDoList/ToDoList/MainWindow.xaml.cs
+++ ToDoList/ToDoList/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//------------------------------------------------
 namespace ToDoList
 {
     /// <summary>
@@ -22,12 +22,43 @@ namespace ToDoList
     public partial class MainWindow : Window
     {
         public ObservableCollection<Task> Tasks { get; set; }
-    
+        //------------------------------------------------    
         public MainWindow()
         {
             InitializeComponent();
 
-            
+            Tasks = new ObservableCollection<Task>();
+        } 
+        //------------------------------------------------
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbName.Text == "" || cbPriority.Text == "" || dpDeadline.Text == "")
+            {
+                tbName.BorderBrush = new SolidColorBrush(Colors.Red);
+                MessageBox.Show("Fill Name, Priority, Deadline");
+            }
+
+            Tasks.Add(new Task
+            {
+                Name = tbName.Text,
+                Priority = cbPriority.Text,
+                Deadline = dpDeadline.Text,
+                Comment = tbComment.Text
+            });
+
+            LastTaskName.Content = tbName.Text;
+            TaskCount.Content = Tasks.Count;
+
+            tbName.Text = "";
+            cbPriority.Text = "";
+            dpDeadline.Text = "";
+            tbComment.Text = "";
+        }
+        //------------------------------------------------
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+//------------------------------------------------
--- ToDoList/ToDoList/Task.cs
+++ ToDoList/ToDoList/Task.cs
@@ -9,7 +9,7 @@ namespace ToDoList
     public class Task
     {
         public string Name { get; set; }
-        public string Priority { get; set; }
+        public string Priority { get; set; } 
         public string Deadline { get; set; }
         public string Comment { get; set; }
     }
